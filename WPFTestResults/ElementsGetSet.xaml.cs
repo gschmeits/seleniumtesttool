55f8ae2a92a28605ac29a855b9c11048184c46d3
Ger <ger.schmeits@home.nl>
2022-06-17 13:07:56 +0200

    datagrid aangepast bij get elements

--- WPFTestResults/ElementsGetSet.xaml.cs
+++ WPFTestResults/ElementsGetSet.xaml.cs
@@ -184,8 +184,8 @@ namespace WPFTestResults
             var borderInlogMargin = BorderInlog.Margin;
             borderInlogMargin.Bottom = 307.0;
             DataGridElements.Visibility = Visibility.Visible;
-            DataGridElements.Height = Height*0.6;
-            DataGridElements.Width = Width*0.8;
+            DataGridElements.Height = Height*0.75;
+            DataGridElements.Width = Width*0.85;
             DataGridElements.Visibility = Visibility.Hidden;
             CheckTextAll.Visibility = Visibility.Hidden;
             ButtonCSV.Visibility = Visibility.Hidden;
